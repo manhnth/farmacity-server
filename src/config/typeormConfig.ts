455029c067b43aa4505f7194b74baf3dfa953ee6
manhtheN <themanh2k@gmail.com>
2023-01-03 21:25:09 +0700

    commit

--- src/config/typeormConfig.ts
+++ src/config/typeormConfig.ts
@@ -1,20 +1,21 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const DbConfig: TypeOrmModuleOptions =
-  process.env.NODE_ENV === 'production'
-    ? {
-        type: 'postgres',
-        url: process.env.DB_URL!,
-        entities: [__dirname + '/./typeorm/*.entity.{ts,js}'],
-        synchronize: true,
-      }
-    : {
-        type: 'postgres',
-        host: 'localhost',
-        port: 5432,
-        username: 'postgres',
-        password: 'postgres',
-        database: 'localdb1',
-        entities: [__dirname + '/./../typeorm/*.entity.{ts,js}'],
-        synchronize: true,
-      };
+  // process.env.NODE_ENV === 'production'
+  //   ?
+  {
+    type: 'postgres',
+    url: 'postgres://rsycgcxd:5DGSQZh6JveCD9R4ne3G81iE4bFlKZ7A@tiny.db.elephantsql.com/rsycgcxd',
+    entities: [__dirname + '/./../typeorm/*.entity.{ts,js}'],
+    synchronize: true,
+  };
+// : {
+//     type: 'postgres',
+//     host: 'localhost',
+//     port: 5432,
+//     username: 'postgres',
+//     password: 'postgres',
+//     database: 'localdb1',
+//     entities: [__dirname + '/./../typeorm/*.entity.{ts,js}'],
+//     synchronize: true,
+//   };
